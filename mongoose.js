fda424c7ef12539e7b504c77a935cee5d78db461
zulkiflijufri <zulkiflijufri54@gmail.com>
2020-12-16 21:37:25 +0800

    use mongoose

--- mongoose.js
+++ mongoose.js
@@ -1,12 +1,34 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://kifli:12345@localhost:27017?authSource=admin", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+mongoose.connect(
+  "mongodb://kifli:12345@localhost:27017/latihan?authSource=admin",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+// create schema
+const quotesSchema = new mongoose.Schema({
+  word: String,
 });
+
+// create Model for schema
+const Quote = mongoose.model("Quote", quotesSchema);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
 db.once("open", () => {
-  console.log("Database connect");
+  // create new quotes
+  const quote = new Quote({
+    word: "Introduction mongoose V2",
+  });
+
+  // save quote
+  quote.save((err, quote) => {
+    if (err) return console.error(err);
+    console.log(quote);
+  });
+
+  //console.log("Database connect");
 });
